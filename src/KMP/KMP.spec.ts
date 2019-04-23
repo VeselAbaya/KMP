4ac4cd463fb5326814307414adff5556beaa2e75
VeselAbaya <Vandervise465@gmail.com>
2019-04-23 21:50:28 +0300

    stepik test added

--- src/KMP/KMP.spec.ts
+++ src/KMP/KMP.spec.ts
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 import {expect} from 'chai';
 
 import {KMP} from './KMP';
@@ -12,4 +15,21 @@ describe('KMP algorithm', () => {
     const entriesArray: Array<number> | -1 = KMP('abcef', 'abcd');
     expect(entriesArray).to.equal(-1);
   });
+});
+
+describe('KMP algorithm (stepik tests)', () => {
+  let filesName: string[] = fs.readdirSync(path.resolve(__dirname, '../../stepik_tests'));
+  filesName = filesName.filter((fileName) => !fileName.includes('.clue'));
+  filesName.forEach((fileName) => {
+    const testFilePath = path.resolve(__dirname, '../../stepik_tests', fileName);
+    const answerFilePath = path.resolve(__dirname, '../../stepik_tests', `${fileName}.clue`);
+
+    const inputArgs = fs.readFileSync(testFilePath).toString().trim().split('\n');
+    it(`should pass stepik test #${fileName}`, () => {
+      const entriesArray: Array<number> | -1 = KMP(inputArgs[1], inputArgs[0]);
+
+      const testAnswer = fs.readFileSync(answerFilePath).toString().trim();
+      expect(entriesArray.toString()).to.deep.equal(testAnswer);
+    });
+  })
 });
